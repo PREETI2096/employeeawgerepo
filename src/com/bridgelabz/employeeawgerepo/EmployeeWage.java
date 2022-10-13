8ce6faa586f84d21689e7bd6e59ab563c2c94860
PREETI2096 <preetirg96@gmail.com>
2022-10-14 01:58:05 +0530

    [preeti]:added uc3 part time employ wage

--- src/com/bridgelabz/employeeawgerepo/EmployeeWage.java
+++ src/com/bridgelabz/employeeawgerepo/EmployeeWage.java
@@ -6,6 +6,7 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee wage computation program");
 		
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHour = 0;
 		int empWage = 0;
@@ -14,6 +15,10 @@ public class EmployeeWage {
 		
 		if(attendance == IS_FULL_TIME)
 			empHour = 8;
+		
+		else if(attendance == IS_PART_TIME)
+			empHour = 4;
+		
 		else 
 			empHour = 0;
 		empWage = empHour * EMP_RATE_PER_HOUR;
