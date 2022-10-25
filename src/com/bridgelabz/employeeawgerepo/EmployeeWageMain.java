6a8ce2ca866cdd64c94848ca61c84bb2c51fc807
PREETI2096 <preetirg96@gmail.com>
2022-10-26 00:25:29 +0530

    [preeti]:added refactoring of code

---
+++ src/com/bridgelabz/employeeawgerepo/EmployeeWageMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeeawgerepo;
+
+public class EmployeeWageMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation program");
+	       EmployeeWage employeewage = new EmployeeWage();	
+	       employeewage.wage();
+
+	}
+
+}
